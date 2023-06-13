f910c0401f4eb251a25e2ac1778608cb4058bd54
Lee Seng Poh (k22002448) <seng.lee@kcl.ac.uk>
2023-06-14 01:06:21 +0800

    Update Chariot.java
    Added method to get legal moves towards the left

--- src/main/java/game/pieces/Chariot.java
+++ src/main/java/game/pieces/Chariot.java
@@ -1,4 +1,9 @@
-package pieces;
+package game.pieces;
+
+import game.Board;
+import game.Location;
+
+import java.util.ArrayList;
 
 /**
  * Represents game piece Chariot that moves in a straight line.
@@ -7,4 +12,32 @@ package pieces;
  * @version 13-6-2023
  */
 public class Chariot extends Piece{
+    public Chariot(Location location, Board board) {
+        super(location, board);
+    }
+
+    @Override
+    public ArrayList<Location> getMoves() {
+        ArrayList<Location> legalMoves = new ArrayList<>();
+
+
+        return legalMoves;
+    }
+
+    public ArrayList<Location> getLeftMoves() {
+        ArrayList<Location> legalMoves = new ArrayList<>();
+        boolean checking = true;
+        Location possibleLocation = new Location(getLocation().getRow(), getLocation().getColumn() - 1);
+
+        while (checking) {
+            if (getBoard().isEmpty(possibleLocation)) {
+                legalMoves.add(possibleLocation);
+                possibleLocation = new Location(possibleLocation.getRow(), getLocation().getColumn() - 1);
+            } else {
+                checking = false;
+            }
+        }
+
+        return legalMoves;
+    }
 }
