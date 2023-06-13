c1b9677bdb4a9ca5d6b0eb3b15049ba006a7b644
Lee Seng Poh (k22002448) <seng.lee@kcl.ac.uk>
2023-06-13 19:09:36 +0800

    Create Location.java

---
+++ src/main/java/Location.java
@@ -0,0 +1,32 @@
+/**
+ * Represents a location a game piece can be at on the board.
+ *
+ * @author Lee Seng Poh
+ * @version 13-6-2023
+ */
+
+public class Location {
+    private int row;
+    private int column;
+
+    /**
+     * Represents the row and the column on the board.
+     * @param row The row.
+     * @param column The column.
+     */
+    public Location(int row, int column)
+    {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getColumn()
+    {
+        return column;
+    }
+}
