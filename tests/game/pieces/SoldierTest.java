772a0647ee4a693c05b9add230b7083e56361b85
Lee Seng Poh <sengpohlee563@gmail.com>
2023-06-23 22:45:25 +0800

    Create SoldierTest.java

---
+++ tests/game/pieces/SoldierTest.java
@@ -0,0 +1,35 @@
+package game.pieces;
+import game.Board;
+import game.Location;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for Soldier.java.
+ *
+ * @author Lee Seng Poh
+ * @version 23-6-2023
+ */
+
+class SoldierTest {
+
+    private Board board;
+
+    @BeforeEach
+    void setUp()
+    {
+        board = new Board(9, 10);
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        board = null;
+    }
+
+}
