ba6daa4ab2297e5757a9d628c16ff056563a57ae
Lee Seng Poh <sengpohlee563@gmail.com>
2023-06-28 00:26:12 +0800

    Create GeneralTest.java

---
+++ tests/game/pieces/GeneralTest.java
@@ -0,0 +1,21 @@
+package game.pieces;
+import game.Board;
+import game.Location;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for General.java.
+ *
+ * @author Lee Seng Poh
+ * @version 27-6-2023
+ */
+
+class GeneralTest {
+
+}
