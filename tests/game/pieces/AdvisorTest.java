0646901367dcdecaf4df2ccb95bf4b5883f44f4f
Lee Seng Poh <sengpohlee563@gmail.com>
2023-06-29 18:58:10 +0800

    Create AdvisorTest.java

---
+++ tests/game/pieces/AdvisorTest.java
@@ -0,0 +1,20 @@
+package game.pieces;
+import game.Board;
+import game.Location;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for Advisor.java.
+ *
+ * @author Lee Seng Poh
+ * @version 29-6-2023
+ */
+
+class AdvisorTest {
+}
