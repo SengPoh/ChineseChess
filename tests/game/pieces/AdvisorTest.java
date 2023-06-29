d19c01e9e4f4abe39d87dcf39b767f731a4cdad1
Lee Seng Poh <sengpohlee563@gmail.com>
2023-06-29 19:14:22 +0800

    Added getMove tests on AdvisorTest.java

--- tests/game/pieces/AdvisorTest.java
+++ tests/game/pieces/AdvisorTest.java
@@ -114,4 +114,175 @@ class AdvisorTest {
         Advisor advisor2 = new Advisor(board, true);
         assertFalse(advisor1.isSameColor(advisor2));
     }
+
+    @Nested
+    @DisplayName("Tests in palace")
+    class AdvisorPalaceTest {
+        @BeforeEach
+        void setup() {
+            for (int i = 3; i <= 5; i++) {
+                for (int j = 0; j < 3; j++) {
+                    board.setPalace(new Location(i, j), true);
+                }
+            }
+        }
+
+        @Test
+        public void getMoves_EmptyPalace_DiagonalUnblocked()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Location advisorLocation = new Location(4, 1);
+            board.setPiece(advisor, advisorLocation);
+
+            ArrayList<Location> expectedResult = new ArrayList<>(Arrays.asList(
+                    new Location(3, 0),
+                    new Location(3, 2),
+                    new Location(5, 0),
+                    new Location(5, 2)
+            ));
+
+            ArrayList<Location> actualResult = advisor.getMoves();
+            assertEquals(expectedResult.size(), actualResult.size(), "List of moves does not have the same size");
+            assertTrue(expectedResult.containsAll(advisor.getMoves()), "The lists of moves are different");
+        }
+
+        @Test
+        public void getMoves_EmptyPalaceCorner_DiagonalUnblocked()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Location advisorLocation = new Location(3, 0);
+            board.setPiece(advisor, advisorLocation);
+
+            ArrayList<Location> expectedResult = new ArrayList<>();
+            expectedResult.add(new Location(4, 1));
+
+            ArrayList<Location> actualResult = advisor.getMoves();
+            assertEquals(expectedResult.size(), actualResult.size(), "List of moves does not have the same size");
+            assertTrue(expectedResult.containsAll(advisor.getMoves()), "The lists of moves are different");
+        }
+        
+        @Test
+        public void getMoves_AllyBlocking_DiagonalBlocked()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Chariot allyChariot = new Chariot(board, true);
+            Location advisorLocation = new Location(4, 1);
+            Location allyLocation = new Location(3, 0);
+            board.setPiece(advisor, advisorLocation);
+            board.setPiece(allyChariot, allyLocation);
+
+            ArrayList<Location> expectedResult = new ArrayList<>(Arrays.asList(
+                    new Location(3, 2),
+                    new Location(5, 0),
+                    new Location(5, 2)
+            ));
+
+            ArrayList<Location> actualResult = advisor.getMoves();
+            assertEquals(expectedResult.size(), actualResult.size(), "List of moves does not have the same size");
+            assertTrue(expectedResult.containsAll(advisor.getMoves()), "The lists of moves are different");
+        }
+
+        @Test
+        public void getMoves_EnemyBlocking_DiagonalBlocked()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Chariot enemyChariot = new Chariot(board, false);
+            Location advisorLocation = new Location(4, 1);
+            Location enemyLocation = new Location(3, 0);
+            board.setPiece(advisor, advisorLocation);
+            board.setPiece(enemyChariot, enemyLocation);
+
+            ArrayList<Location> expectedResult = new ArrayList<>(Arrays.asList(
+                    new Location(3, 0),
+                    new Location(3, 2),
+                    new Location(5, 0),
+                    new Location(5, 2)
+            ));
+
+            ArrayList<Location> actualResult = advisor.getMoves();
+            assertEquals(expectedResult.size(), actualResult.size(), "List of moves does not have the same size");
+            assertTrue(expectedResult.containsAll(advisor.getMoves()), "The lists of moves are different");
+        }
+        
+        @Test
+        public void getMoves_FullyAllyBlocking_OrthogonalBlocked()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Chariot allyChariot1 = new Chariot(board, true);
+            Chariot allyChariot2 = new Chariot(board, true);
+            Chariot allyChariot3 = new Chariot(board, true);
+            Chariot allyChariot4 = new Chariot(board, true);
+            Location advisorLocation = new Location(4, 1);
+            Location allyLocation1 = new Location(3, 0);
+            Location allyLocation2 = new Location(3, 2);
+            Location allyLocation3 = new Location(5, 0);
+            Location allyLocation4 = new Location(5, 2);
+            board.setPiece(advisor, advisorLocation);
+            board.setPiece(allyChariot1, allyLocation1);
+            board.setPiece(allyChariot2, allyLocation2);
+            board.setPiece(allyChariot3, allyLocation3);
+            board.setPiece(allyChariot4, allyLocation4);
+
+            ArrayList<Location> expectedResult = new ArrayList<>();
+
+            ArrayList<Location> actualResult = advisor.getMoves();
+            assertEquals(expectedResult.size(), actualResult.size(), "List of moves does not have the same size");
+            assertTrue(expectedResult.containsAll(advisor.getMoves()), "The lists of moves are different");
+        }
+
+        @Test
+        public void getMoves_FullEnemyBlocking_OrthogonalCapture()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Chariot enemyChariot1 = new Chariot(board, false);
+            Chariot enemyChariot2 = new Chariot(board, false);
+            Chariot enemyChariot3 = new Chariot(board, false);
+            Chariot enemyChariot4 = new Chariot(board, false);
+            Location advisorLocation = new Location(4, 1);
+            Location enemyLocation1 = new Location(3, 0);
+            Location enemyLocation2 = new Location(3, 2);
+            Location enemyLocation3 = new Location(5, 0);
+            Location enemyLocation4 = new Location(5, 2);
+            board.setPiece(advisor, advisorLocation);
+            board.setPiece(enemyChariot1, enemyLocation1);
+            board.setPiece(enemyChariot2, enemyLocation2);
+            board.setPiece(enemyChariot3, enemyLocation3);
+            board.setPiece(enemyChariot4, enemyLocation4);
+
+            ArrayList<Location> expectedResult = new ArrayList<>(Arrays.asList(
+                    new Location(3, 0),
+                    new Location(3, 2),
+                    new Location(5, 0),
+                    new Location(5, 2)
+            ));
+
+            ArrayList<Location> actualResult = advisor.getMoves();
+            assertEquals(expectedResult.size(), actualResult.size(), "List of moves does not have the same size");
+            assertTrue(expectedResult.containsAll(advisor.getMoves()), "The lists of moves are different");
+        }
+
+        @Test
+        public void move_LegalMove_True()
+        {
+            Advisor advisor = new Advisor(board, true);
+            Location location = new Location(4, 1);
+            board.setPiece(advisor, location);
+            Location newLocation = new Location(3,0);
+
+            assertTrue(advisor.move(newLocation));
+            assertEquals(newLocation, advisor.getLocation(), "This advisor was not moved to the correct location.");
+        }
+
+        @Test
+        public void move_IllegalMove_False() {
+            Advisor advisor = new Advisor(board, true);
+            Location location = new Location(4, 1);
+            board.setPiece(advisor, location);
+
+            Location newLocation = new Location(2, 1);
+
+            assertFalse(advisor.move(newLocation));
+            assertEquals(location, advisor.getLocation(), "This advisor's location was changed.");
+        }
+    }
 }
