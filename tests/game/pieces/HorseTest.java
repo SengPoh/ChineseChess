e8c190628ff833e647af1c58e1b207e24b8f7b07
Lee Seng Poh <sengpohlee563@gmail.com>
2023-06-30 20:42:30 +0800

    Create HorseTest.java

---
+++ tests/game/pieces/HorseTest.java
@@ -0,0 +1,22 @@
+package game.pieces;
+import game.Board;
+import game.Location;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for Horse
+ * .java.
+ *
+ * @author Lee Seng Poh
+ * @version 30-6-2023
+ */
+
+class HorseTest {
+
+}
