50b9e15906bce71c471f99aa2b1c6198730da4f4
Lee Seng Poh <sengpohlee563@gmail.com>
2023-06-30 20:44:15 +0800

    Added basic tests to HorseTest.java

--- tests/game/pieces/HorseTest.java
+++ tests/game/pieces/HorseTest.java
@@ -18,5 +18,105 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 class HorseTest {
+    private Board board;
 
+    @BeforeEach
+    void setUp()
+    {
+        board = new Board(9, 10);
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        board = null;
+    }
+
+    @Test
+    public void init() {
+        Horse horse = new Horse(board, true);
+
+        assertEquals(board, horse.getBoard(), "the board is different from expected");
+        assertTrue(horse.isBlack(), "the color is different from expected");
+
+        ArrayList<Location> expectedMoveSet = new ArrayList<>(Arrays.asList(
+                new Location(1, 2),
+                new Location(1, -2),
+                new Location(-1, 2),
+                new Location(-1, -2),
+                new Location(2, 1),
+                new Location(2, -1),
+                new Location(-2, 1),
+                new Location(-2, -1)
+        ));
+        ArrayList<Location> actualMoveSet = horse.getMoveSet();
+        assertTrue(actualMoveSet.containsAll(expectedMoveSet), "The move sets are different");
+    }
+
+    @Test
+    public void init_NullBoard_Exception()
+    {
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> new Horse(null, true));
+        assertEquals("A piece must be assigned to a board.", exception.getMessage());
+    }
+
+    @Test
+    public void setLocation_Null_Works()
+    {
+        Horse horse = new Horse(board, true);
+        horse.setLocation(null);
+        assertNull(horse.getLocation());
+    }
+
+    @Test
+    public void setLocation_InvalidLocation_Exception()
+    {
+        Horse horse = new Horse(board, true);
+
+        Location invalidLocation = new Location(-1, -1);
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> horse.setLocation(invalidLocation));
+        assertEquals("Location can only be either within the board or null.", exception.getMessage());
+    }
+
+    @Test
+    public void isBlack_Black_True()
+    {
+        Horse horse = new Horse(board, true);
+
+        assertTrue(horse.isBlack());
+    }
+
+    @Test
+    public void isBlack_White_False()
+    {
+        Horse horse = new Horse(board, false);
+
+        assertFalse(horse.isBlack());
+    }
+
+    @Test
+    public void isSameColor_BothBlack_True()
+    {
+        Horse horse1 = new Horse(board, true);
+        Horse horse2 = new Horse(board, true);
+        assertTrue(horse1.isSameColor(horse2));
+    }
+
+    @Test
+    public void isSameColor_BothWhite_True()
+    {
+        Horse horse1 = new Horse(board, false);
+        Horse horse2 = new Horse(board, false);
+        assertTrue(horse1.isSameColor(horse2));
+    }
+
+    @Test
+    public void isSameColor_BlackAndWhite_False()
+    {
+        Horse horse1 = new Horse(board, false);
+        Horse horse2 = new Horse(board, true);
+        assertFalse(horse1.isSameColor(horse2));
+    }
 }
