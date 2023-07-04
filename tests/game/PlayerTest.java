a95ed2c217de8b07f3806cbebb5a9183379c647f
Lee Seng Poh <sengpohlee563@gmail.com>
2023-07-04 09:40:19 +0800

    Update PlayerTest.java

--- tests/game/PlayerTest.java
+++ tests/game/PlayerTest.java
@@ -1,7 +1,27 @@
 package game;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlayerTest {
+/**
+ * Tests for Player.java.
+ * @author Lee Seng Poh
+ * @version 28-6-2023
+ */
 
+class PlayerTest {
+    @Test
+    public void isBlack_BlackPlayer_True()
+    {
+        Player player = new Player(true);
+        assertTrue(player.isBlack());
+    }
+
+    @Test
+    public void isBlack_NotBlackPlayer_False()
+    {
+        Player player = new Player(false);
+        assertFalse(player.isBlack());
+    }
 }
