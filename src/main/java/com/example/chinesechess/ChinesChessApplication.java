4a3a96b08d402c681a7b7b5c3a627d73acca24ae
Lee Seng Poh <sengpohlee563@gmail.com>
2023-07-11 23:37:38 +0800

    Update ChinesChessApplication.java
    method to populate circles representing the locations on the board

--- src/main/java/com/example/chinesechess/ChinesChessApplication.java
+++ src/main/java/com/example/chinesechess/ChinesChessApplication.java
@@ -1,12 +1,15 @@
 package com.example.chinesechess;
 
+import game.Location;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -17,6 +20,8 @@ import javafx.stage.StageStyle;
  * @version 10-7-2023
  */
 public class ChinesChessApplication extends Application {
+    public static final double INIT_BOARD_WIDTH = 300.0;
+    public static final double INIT_LOCATION_RADIUS = 13.0;
 
     public static void main(String[] args)
     {
@@ -30,6 +35,7 @@ public class ChinesChessApplication extends Application {
 
         StackPane root = new StackPane();
         root.getChildren().add(boardImage);
+        populateBoardLocations(root);
 
         Scene scene = new Scene(root);
 
@@ -57,7 +63,38 @@ public class ChinesChessApplication extends Application {
         ImageView boardImage = new ImageView();
         boardImage.setImage(image);
         boardImage.setPreserveRatio(true);
-        boardImage.setFitWidth(300);
+        boardImage.setFitWidth(INIT_BOARD_WIDTH);
         return boardImage;
+    }
+
+    private void populateBoardLocations(Pane parent)
+    {
+        Pane anchorPane = new Pane();
+        parent.getChildren().add(anchorPane);
+
+        double ratio = INIT_LOCATION_RADIUS / INIT_BOARD_WIDTH;
+        //spacing between each circle
+        double spacingX = 33.0;
+        double spacingY = 34.0;
+        //x and y coordinates of the circle
+        double initialX = 17.0;
+        double initialY = 15.0;
+        double currentX = initialX;
+        double currentY = initialY;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 10; j++) {
+                Circle circle = new Circle(INIT_LOCATION_RADIUS);
+                circle.setLayoutX(currentX);
+                circle.setLayoutY(currentY);
+                anchorPane.getChildren().add(circle);
+
+                circle.radiusProperty().bind(parent.heightProperty().multiply(ratio));
+
+                currentY = currentY + spacingY;
+            }
+            currentX = currentX + spacingX;
+            currentY = initialY;
+        }
     }
 }
