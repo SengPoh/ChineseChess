40dd6fda885878a956f849c3b0628701fe143a2b
Lee Seng Poh <sengpohlee563@gmail.com>
2023-08-04 00:41:44 +0800

    Create PlayerTilePane.java

---
+++ src/main/java/GUI/PlayerTilePane.java
@@ -0,0 +1,24 @@
+package GUI;
+
+import game.Player;
+import javafx.scene.layout.TilePane;
+
+/**
+ * A TilePane that contains reference to a player. This is designed to be used as the menu bar of a player.
+ *
+ * @author Lee Seng Poh
+ * @version 4-8-2023
+ */
+public class PlayerTilePane extends TilePane {
+    private Player player;
+
+    public PlayerTilePane(Player player)
+    {
+        super();
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
