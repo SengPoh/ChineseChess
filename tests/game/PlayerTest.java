5ecbbfb4a5862b9c018e9b71493e9a8a26915a46
Lee Seng Poh <sengpohlee563@gmail.com>
2023-08-05 17:45:08 +0800

    Update PlayerTest.java

--- tests/game/PlayerTest.java
+++ tests/game/PlayerTest.java
@@ -1,5 +1,6 @@
 package game;
 
+import game.pieces.Chariot;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +24,21 @@ class PlayerTest {
     {
         Player player = new Player(false);
         assertFalse(player.isBlack());
+    }
+
+    @Test
+    public void recordMove_LocationWithPiece_recorded()
+    {
+        Player player = new Player(false);
+        Location moveFromLocation = new Location(4, 6);
+        Chariot chariot = new Chariot(new Board(9, 10), false);
+        moveFromLocation.setPiece(chariot);
+        Location moveToLocation = new Location(6, 6);
+        player.recordMove(moveFromLocation, moveToLocation);
+
+        Location[] move = player.popPreviousMove();
+        assertEquals(move[0], moveFromLocation);
+        assertEquals(move[1], moveToLocation);
+        assertTrue(move[0].getPiece() instanceof Chariot);
     }
 }
