f5279f3b50bd371be4a8c7d31b3622683953f538
Lee Seng Poh <sengpohlee563@gmail.com>
2023-08-27 09:19:59 +0800

    Update MoveSearchAlgorithm.java

--- src/main/java/game/MoveSearchAlgorithm.java
+++ src/main/java/game/MoveSearchAlgorithm.java
@@ -7,16 +7,20 @@ public class MoveSearchAlgorithm {
     private Game game;
     //how many moves this algorithm looks ahead.
     private int ply;
+    private boolean computerPlayerIsBlack;
 
-    public MoveSearchAlgorithm(Game game, int ply)
+    public MoveSearchAlgorithm(Game game, int ply, boolean computerPlayerIsBlack)
     {
         this.game = game;
         this.ply = ply;
+        this.computerPlayerIsBlack = computerPlayerIsBlack;
     }
 
-    public int alphaBeta()
+    public int alphaBeta(int alpha, int beta, int depth, boolean isMaximizingPlayer)
     {
-        int value = 0;
-        return value;
+        if (depth == 0) {
+            return game.evaluateScore(computerPlayerIsBlack);
+        }
+        return 0;
     }
 }
