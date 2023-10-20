d6a633d97b358da79e53ed85f5a6792b7b29b1f8
Lee Seng Poh <sengpohlee563@gmail.com>
2023-10-20 19:51:01 +0100

    Create main menu

--- src/main/java/GUI/ChineseChessApplication.java
+++ src/main/java/GUI/ChineseChessApplication.java
@@ -1,7 +1,12 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ChineseChessApplication extends Application {
@@ -15,8 +20,23 @@ public class ChineseChessApplication extends Application {
     {
 
         primaryStage.setTitle("Xiang Qi");
-        createGamePane(primaryStage);
+        createMainMenu(primaryStage);
 
+    }
+
+    private void createMainMenu(Stage primaryStage)
+    {
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setId("main-menu");
+        Button onePlayerButton = new Button("One Player");
+        Button twoPlayersButton = new Button("Two Players");
+        root.getChildren().addAll(onePlayerButton, twoPlayersButton);
+
+        Scene scene = new Scene(root, 300, 400);
+        scene.getStylesheets().add(ChineseChessApplication.class.getResource("/StyleSheet.css").toString());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     private void createGamePane(Stage primaryStage)
